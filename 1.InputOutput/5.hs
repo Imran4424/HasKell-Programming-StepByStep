d42ccadcfb596749ab678e2bbfe6037adc1a3925
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-08 23:49:52 +0600

    input output

--- 1.InputOutput/5.hs
+++ 1.InputOutput/5.hs
@@ -1,2 +1,8 @@
 -- Write a program that read and display floating point number
 
+module Main where
+
+main = do 
+	putStrLn "Enter an floatint point number"
+	number <- readLn :: IO Double
+	putStrLn ("You entered: " ++ show number)
