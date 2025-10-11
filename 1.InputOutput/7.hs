70e386beef6c38418e8f4c228a414274c7258b8e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 10:34:09 +0600

    input output

--- 1.InputOutput/7.hs
+++ 1.InputOutput/7.hs
@@ -1,2 +1,8 @@
 -- Write a program that read and display any character
 
+module Main where
+
+main = do
+	putStrLn "Enter a character"
+	char <- getChar -- reads exactly one character
+	putStrLn ("You entered: " ++ [char])
