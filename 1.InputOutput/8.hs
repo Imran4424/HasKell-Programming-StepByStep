5ea58c14432aba7d2c58ebdbf3831a7f25ffd796
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 11:24:15 +0600

    input output

--- 1.InputOutput/8.hs
+++ 1.InputOutput/8.hs
@@ -2,4 +2,11 @@
 
 module Main where
 
-main = putStrLn "Hello World"
+main = do
+	putStrLn "Enter an ASCII value (0-255):"
+
+	number <- readLn :: IO Int
+	if number >= 0 && number <= 255 then
+		putStrLn ("Equivalent character: " ++ [chr number])
+	else
+		putStrLn "Out of ASCII range (0-255)."
