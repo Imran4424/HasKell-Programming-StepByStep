c9dfab013880a336c625e4c834103bd05c8dd340
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 13:15:02 +0600

    input output

--- 1.InputOutput/12.hs
+++ 1.InputOutput/12.hs
@@ -1,2 +1,20 @@
 -- Write a program that declares and uses any Bool literals
 
+module Main where
+
+main = do
+  -- Boolean value initialization
+  let examResult0 :: Bool
+      examResult0 = False
+  print examResult0
+
+  let mark1 :: Int
+      mark1 = 80
+      -- result of the condition will be a boolean value, which is True
+      examResult1 = mark1 >= 40
+  print examResult1
+
+  let mark2 = 33
+      -- result of the condition will be a boolean value, which is False
+      examResult2 = mark2 >= 40
+  print examResult2
