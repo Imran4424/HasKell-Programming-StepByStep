66fe23d484daf4651f4f1e13483f421252ec73ce
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 14:41:56 +0600

    operators

--- 2.Operators/1.hs
+++ 2.Operators/1.hs
@@ -1,1 +1,10 @@
 -- Write a program that read two integer and display sum
+
+module Main where
+
+main = do
+	putStrLn "Enter first integer:"
+	a <- readLn :: IO Int
+	putStrLn "Enter second integer:"
+	b <- readLn :: IO Int
+	putStrLn ("Sum = " ++ show (a + b))
