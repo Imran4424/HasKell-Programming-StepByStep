aee8dddccd72ea47272377d221f78a4bf52cb307
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 13:57:31 +0600

    input output

--- 1.InputOutput/12.1.hs
+++ 1.InputOutput/12.1.hs
@@ -4,9 +4,23 @@ module Main where
 
 import Data.IORef
 
+main :: IO ()
 main = do
-  mark <- newIORef (80 :: Int)
-  examResult <- newIORef False
+  -- Declare once
+  mark        <- newIORef (80 :: Int)
+  examResult  <- newIORef False
 
-  printRef examResult          -- prints False
-  printRef mark                -- prints 80
+  -- Use 1
+  readIORef examResult >>= print         -- False
+
+  -- Reuse: compute from current mark (80)
+  m1 <- readIORef mark
+  writeIORef examResult (m1 >= 40)
+  readIORef examResult >>= print         -- True
+
+  -- Reuse: change mark to 33, recompute
+  writeIORef mark 33
+  m2 <- readIORef mark
+  writeIORef examResult (m2 >= 40)
+  readIORef examResult >>= print         -- False
+
