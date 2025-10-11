67f12238bfdbf3785013957748a70de8f200c3b6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 14:38:05 +0600

    input output

--- 1.InputOutput/13.hs
+++ 1.InputOutput/13.hs
@@ -1,2 +1,8 @@
 -- Write a program that read and display your name
 
+module Main where
+
+main = do
+	putStrLn "Enter your name:"
+	name <- getLine           -- reads a whole line of text
+	putStrLn ("You entered: " ++ name)
