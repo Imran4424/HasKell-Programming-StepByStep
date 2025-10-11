232a3f4ac8d6167e99fe87ff03ee10ac3252ceff
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 13:07:25 +0600

    input output

--- 1.InputOutput/10.hs
+++ 1.InputOutput/10.hs
@@ -1,1 +1,10 @@
 -- Write a program that read any lower case character and display in upper case
+
+module Main where
+
+import Data.Char
+
+main = do
+	putStrLn "Enter a character"
+	char <- getChar -- reads exactly one character
+	putStrLn ("Equivalent ASCII value: " ++ show (ord char))
