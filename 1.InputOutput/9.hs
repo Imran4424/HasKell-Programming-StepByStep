e8b5218d94b6ff5c737f8d6557ed15a9470f93d9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-11 13:01:53 +0600

    input output

--- 1.InputOutput/9.hs
+++ 1.InputOutput/9.hs
@@ -1,1 +1,10 @@
 -- Wrtie a program that read any character and display equivalent ASCII value
+
+module Main where
+
+import Data.Char
+
+main = do
+	putStrLn "Enter a character"
+	char <- getChar -- reads exactly one character
+	putStrLn ("Equivalent ASCII value: " ++ show (ord char))
