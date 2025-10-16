a2eb1ff6a380c03d05ad0835b4e74fdf597d8bea
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 00:13:58 +0600

    operators

--- 2.Operators/14.hs
+++ 2.Operators/14.hs
@@ -1,2 +1,11 @@
 -- Write a program that read a number and mod by four using bitwise AND
 
+module Main where
+
+import Data.Bits
+
+main = do
+	putStrLn "Enter an Integer: "
+	number <- readLn :: IO Int
+	let multiplyByFive = number .<<. 2 + number
+	putStrLn ("number << 2 = " ++ show multiplyByFive)
