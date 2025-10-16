620b21519625e8de0f17d65e394bb06ae88ae339
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-16 23:53:00 +0600

    operators

--- 2.Operators/10.hs
+++ 2.Operators/10.hs
@@ -1,1 +1,10 @@
 -- Write a program that read two numbers and display bitwise Exclusive OR
+
+import Data.Bits
+
+main = do
+	putStrLn "Enter first Integer: "
+	x <- readLn :: IO Int
+	putStrLn "Enter second Integer: "
+	y <- readLn :: IO Int
+	putStrLn ("Bitwise AND = " ++ show (x .^. y))
