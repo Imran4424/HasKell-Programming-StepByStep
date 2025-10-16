c5a407fb4d1be852efa31d6ef6af80804147e0f7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-16 23:41:16 +0600

    operators

--- 2.Operators/7.hs
+++ 2.Operators/7.hs
@@ -10,3 +10,9 @@ main = do
 	putStrLn $ "unary -number = " ++ show (-number)
 	putStrLn $ "negate number = " ++ show (negate number)
 	putStrLn $ "Is unary - equal to negate - ? " ++ show ((-number) == negate number)
+
+
+	-- Precedence demo: exponentiation binds tighter than unary minus
+	-- So -number^2 is parsed as -(number^2), not (-number)^2.
+	putStrLn $ "-(number^2)      = " ++ show (-(number^2))
+	putStrLn $ "(-number)^2      = " ++ show ((-number) ^ 2)
