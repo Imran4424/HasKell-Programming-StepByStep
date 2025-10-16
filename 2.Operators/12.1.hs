7c265a63d879c4b5879766f0e51380482993aa05
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 00:06:50 +0600

    operators

--- 2.Operators/12.1.hs
+++ 2.Operators/12.1.hs
@@ -8,5 +8,5 @@ main = do
 	putStrLn "Enter an Integer: "
 	number <- readLn :: IO Int
 	-- divide by two using right shift by 1
-	let multiplyByTwo = number `shiftL` 1
+	let multiplyByTwo = number .<<. 1
 	putStrLn ("number << 1 = " ++ show multiplyByTwo)
