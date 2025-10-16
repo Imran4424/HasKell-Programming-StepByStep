eaaa0073e994beb9936483daed5d89b2134cf164
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-16 23:31:05 +0600

    operators

--- 2.Operators/7.hs
+++ 2.Operators/7.hs
@@ -1,2 +1,12 @@
 -- Read an integer, print its negation using unary - and compare with negate 
 -- (explain difference in precedence).
+
+module Main where
+
+main = do
+	putStrLn "Enter an integer:"
+	number <- readLn :: IO Int
+
+	putStrLn ("unary -number = ", ++ show (-number))
+	putStrLn ("negate number = ", ++ show (negate number))
+	putStrLn ("Is unary - equal to negate - = ", ++ show (negate number))
