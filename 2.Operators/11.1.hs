85de2436a10dcc0b3604d9737534cad4798d00a3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 00:03:47 +0600

    operators

---
+++ 2.Operators/11.1.hs
@@ -0,0 +1,12 @@
+-- Write a program that read a number and divide by two using shift operator
+
+module Main where
+
+import Data.Bits
+
+main = do
+	putStrLn "Enter an Integer: "
+	number <- readLn :: IO Int
+	-- divide by two using right shift by 1
+	let half = number .>>. 1
+	putStrLn ("number >> 1 = " ++ show half)
