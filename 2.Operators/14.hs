9cc87973ed5ef2f6b9f861c0397206aeeeb9771b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 00:14:33 +0600

    operators

--- 2.Operators/14.hs
+++ 2.Operators/14.hs
@@ -7,5 +7,5 @@ import Data.Bits
 main = do
 	putStrLn "Enter an Integer: "
 	number <- readLn :: IO Int
-	let multiplyByFive = number .<<. 2 + number
+	let multiplyByFive = number .&. 4
 	putStrLn ("number << 2 = " ++ show multiplyByFive)
