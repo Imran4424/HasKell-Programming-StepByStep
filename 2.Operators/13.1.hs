e2d264297ad74622e8fb1fc1502cd5c5787de476
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 00:12:13 +0600

    operators

---
+++ 2.Operators/13.1.hs
@@ -0,0 +1,11 @@
+-- Write a program that read a number and multiply by five using shift operator
+
+module Main where
+
+import Data.Bits
+
+main = do
+	putStrLn "Enter an Integer: "
+	number <- readLn :: IO Int
+	let multiplyByFive = number .<<. 2 + number
+	putStrLn ("number << 2 = " ++ show multiplyByFive)
