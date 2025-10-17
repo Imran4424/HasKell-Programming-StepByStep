63fb049851c46db7de4ea8a63dd4601761c5587e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 21:25:04 +0600

    operator

--- 2.Operators/15.hs
+++ 2.Operators/15.hs
@@ -1,1 +1,12 @@
 -- Read two integers and print the larger using max and compare with using >
+
+module Main where
+
+main = do
+	putStrLn "Enter first integer:"
+	x <- readLn :: IO Int
+	putStrLn "Enter second integer:"
+	y <- readLn :: IO Int
+
+	let maxValue = max x y
+	putStrLn ("Large value: " ++ show maxValue)
