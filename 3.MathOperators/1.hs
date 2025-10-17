a6449f77345351ef74f04db4d34aadd439665ec8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 21:36:50 +0600

    operator

--- 3.MathOperators/1.hs
+++ 3.MathOperators/1.hs
@@ -1,1 +1,8 @@
 -- Write a program that read any number and display absolute value
+
+module Main where
+
+main = do
+	putStrLn "Enter a number:"
+	number <- readLn :: IO Double
+	putStrLn ("Absolute value: " ++ show (abs number))
