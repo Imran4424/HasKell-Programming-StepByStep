f616b08128f069b5ed379f575062a06c21ce6a02
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:19:29 +0600

    operator

--- 2.Operators/16.hs
+++ 2.Operators/16.hs
@@ -1,1 +1,9 @@
 -- Write a program that read two numbers and display maximum using ternary operator
+
+module Main where
+
+main = do
+	putStrLn "Enter first integer:"
+	x <- readLn :: IO Int
+	putStrLn "Enter second integer:"
+	y <- readLn :: IO Int
