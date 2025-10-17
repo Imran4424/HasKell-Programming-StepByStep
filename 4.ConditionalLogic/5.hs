8fe38d8103b12421860f1a0ebadc6e8f5478a5d5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:27:21 +0600

    conditional logic

--- 4.ConditionalLogic/5.hs
+++ 4.ConditionalLogic/5.hs
@@ -1,2 +1,11 @@
 -- Write a program that read three numbers and display maximum
 
+module Main where
+
+main = do
+	putStrLn "Enter first number:"
+	x <- readLn :: IO Double
+	putStrLn "Enter second number:"
+	y <- readLn :: IO Double
+	putStrLn "Enter third number:"
+	z <- readLn :: IO Double
