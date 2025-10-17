2f4a451655174939c84e7259e09a880891d4aa2f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:28:43 +0600

    conditional logic

--- 4.ConditionalLogic/6.hs
+++ 4.ConditionalLogic/6.hs
@@ -1,1 +1,12 @@
 -- Write a program that read three numbers and display minimum
+
+module Main where
+
+main = do
+	putStrLn "Enter first number:"
+	x <- readLn :: IO Double
+	putStrLn "Enter second number:"
+	y <- readLn :: IO Double
+	putStrLn "Enter third number:"
+	z <- readLn :: IO Double
+	putStrLn ("Maximum = " ++ show (minimum [x, y, z]))
