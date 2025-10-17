348733d216268af6d274df267d64a1a00905d877
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:40:30 +0600

    conditional logic

--- 4.ConditionalLogic/8.hs
+++ 4.ConditionalLogic/8.hs
@@ -3,6 +3,21 @@
 module Main where
 
 main = do
-	putStrLn "Enter a number: "
-	number <- readLn :: IO Double
-	
+	putStrLn "Enter the mark: "
+	mark <- readLn :: IO Double
+	if mark > 100 || mark < 0 then
+		putStrLn "Invalid mark (must be 0–100)"
+	else if mark >= 80 then
+		putStrLn "A+"
+	else if mark >= 70 then
+		putStrLn "A"
+	else if mark >= 65 then
+		putStrLn "A-"
+	else if mark >= 60 then
+		putStrLn "B"
+	else if mark >= 50 then
+		putStrLn "C"
+	else if mark >= 40 then
+		putStrLn "D"
+	else
+		putStrLn "F"
