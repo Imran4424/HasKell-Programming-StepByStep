88670af8af656c1888a825f924db0b859422c54f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:15:05 +0600

    conditional logic

--- 4.ConditionalLogic/1.hs
+++ 4.ConditionalLogic/1.hs
@@ -1,2 +1,11 @@
 -- Write a program that read an integer and prints odd or even
 
+module Main where
+
+main = do
+	putStrLn "Enter an Integer: "
+	number <- readLn :: IO Int
+	if number mod 2 == 0 then
+		putStrLn "Even"
+	else
+		putStrLn "Odd"
