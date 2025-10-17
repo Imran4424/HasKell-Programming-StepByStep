d626017a517f43d3521598a7b7f3760b699a4e48
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:25:22 +0600

    conditional logic

--- 4.ConditionalLogic/4.hs
+++ 4.ConditionalLogic/4.hs
@@ -1,1 +1,10 @@
 -- Write a program that read two numbers and display minimum
+
+module Main where
+
+main = do
+	putStrLn "Enter first number:"
+	x <- readLn :: IO Double
+	putStrLn "Enter second number:"
+	y <- readLn :: IO Double
+	putStrLn ("Maximum = " ++ show (max a b))
