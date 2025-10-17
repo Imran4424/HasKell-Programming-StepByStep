d227e3e4402e21b673a320421933f55cc9c28a16
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 22:42:51 +0600

    operator

--- 3.MathOperators/5.hs
+++ 3.MathOperators/5.hs
@@ -1,2 +1,7 @@
 -- Write a program that read any number and display it's square root
 
+module Main where
+
+main = do -- do is needed for multiple statements
+	putStrLn "Enter a number:"
+	number <- readLn :: IO Double
