269c0126b61d064755f9222240f3af499767d2c0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-17 23:32:59 +0600

    conditional logic

--- 4.ConditionalLogic/7.hs
+++ 4.ConditionalLogic/7.hs
@@ -1,1 +1,12 @@
 -- Write a program that read three numbers and display median
+
+module Main where
+
+main = do
+	putStrLn "Enter first number:"
+	x <- readLn :: IO Double
+	putStrLn "Enter second number:"
+	y <- readLn :: IO Double
+	putStrLn "Enter third number:"
+	z <- readLn :: IO Double
+	putStrLn ("Median = " ++ show (x + y + z - maximum [x, y, z] - minimum [x, y, z]))
