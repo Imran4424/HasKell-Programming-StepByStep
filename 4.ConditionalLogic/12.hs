1b5a5d1024dd17ec7012e5a58756a9cc54c547b6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-18 18:37:02 +0600

    conditional logic

--- 4.ConditionalLogic/12.hs
+++ 4.ConditionalLogic/12.hs
@@ -1,1 +1,9 @@
 -- Write a program to check whether a character is uppercase or lowercase alphabet
+
+module Main where
+
+import Data.Char
+
+main = do
+	putStrLn "Enter the uppercase character"
+	char <- getChar -- reads exactly one character
