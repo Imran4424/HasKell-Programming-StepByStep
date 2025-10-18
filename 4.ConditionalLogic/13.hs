cec6c4dc6e9364304b385256ea95b154112819b8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-18 18:42:15 +0600

    conditional logic

--- 4.ConditionalLogic/13.hs
+++ 4.ConditionalLogic/13.hs
@@ -1,9 +1,9 @@
 -- Write a program to generate a simple arithmetic calculator
-    -- hints:
-    --       enter two numbers: 6 5
-    --       select the menu:
-    --          1. Add
-    --          2. Subtract
-    --          3. Multiply
-    --          4. Divide
+-- hints:
+--       enter two numbers: 6 5
+--       select the menu:
+--          1. Add
+--          2. Subtract
+--          3. Multiply
+--          4. Divide
 
