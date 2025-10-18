9277254581283f1ba5f5401df651b1f062663906
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-18 15:57:12 +0600

    conditional logic

--- 4.ConditionalLogic/9.hs
+++ 4.ConditionalLogic/9.hs
@@ -5,3 +5,14 @@ module Main where
 main = do
 	putStrLn "Enter the year: "
 	year <- readLn :: IO Int
+
+	if year `mod` 4 == 0 then
+		if year `mod` 100 == 0 then
+			if year `mod` 400 == 0 then
+				putStrLn "Leap Year"
+			else
+				putStrLn "Not Leap Year"
+		else
+			putStrLn "Leap Year"
+	else
+		putStrLn "Not Leap Year"
